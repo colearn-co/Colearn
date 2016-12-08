33f8034887b5634c13be38f58fb509d4e0456b8f
kamal <kkjoshi.it@gmail.com>
2016-12-08 14:24:51 +0530

    few improvemt and user area methods

--- app/assets/javascripts/nChat.js
+++ app/assets/javascripts/nChat.js
@@ -1,6 +1,9 @@
-function Chat(user) {
-	this.user = user;
+function Chat(currentUser, users, newMsgCallback) {
+	this.currentUser = currentUser;
+	this.users = users;
+	this.newMsgCallback = newMsgCallback;
 	var $chatArea     = $('.chat-area'),
+		$userArea = $('.user-area'),
 	    $printer  = $('.messages'),
 	    $textArea = $('.message-input-area'),
 	    printerH  = $printer.innerHeight(),
@@ -14,19 +17,21 @@ function Chat(user) {
 		$printer.stop().animate( {scrollTop: $printer[0].scrollHeight - printerH  }, 600); // SET SCROLLER TO BOTTOM
 	  }
 	}	
-	scrollBottom(); // DO IMMEDIATELY
-
+	scrollBottom(); 
 	function postMessage(e){  
 	  // on Post click or 'enter' but allow new lines using shift+enter
-	  if(e.type=='click' || (e.which==13 && !e.shiftKey)){ 
+	  if (e.type=='click' || (e.which==13 && !e.shiftKey)) { 
 	    e.preventDefault();
 	    var msg = $textArea.val(); // not empty / space
-	    if($.trim(msg)){
+	    if($.trim(msg)) {
 	      var currentMsg = new Message(msg, new Date().getTime());
-	      addMessage(user, currentMsg);
+	      if (newMsgCallback) {
+	      	newMsgCallback(currentMsg);
+	      }
+	      addMessage(currentUser, currentMsg);
 	      $textArea[0].value=''; // CLEAR TEXTAREA
 	      scrollBottom(); // DO ON POST
-	      // HERE Use AJAX to post msg to PHP      
+	        
 	    } 
 	  }
 	}
@@ -35,7 +40,7 @@ function Chat(user) {
 	//// PREVENT SCROLL TO BOTTOM WHILE READING OLD MESSAGES
 	$printer.hover(function( e ) {
 	  preventNewScroll = e.type=='mouseenter' ? true : false ;
-	  if(!preventNewScroll){ scrollBottom(); } // On mouseleave go to bottom
+	  if (!preventNewScroll) { scrollBottom(); } // On mouseleave go to bottom
 	});
 
 	$textArea.keyup(postMessage);
@@ -43,17 +48,25 @@ function Chat(user) {
 	//// TEST ONLY - SIMULATE NEW MESSAGES
 	var i = 0;
 	intv = setInterval(function(){
-		var user = new User("kamal", "http://gravatar.com/avatar/4cc30665303007ca1b503141d3f85858.jpg?d=monsterid" );
+		var user = new User({
+			name: "kamal", 
+			picture: "http://gravatar.com/avatar/4cc30665303007ca1b503141d3f85858.jpg?d=monsterid" 
+		});
 		console.log("user:" + user.getUserHTML());
 	    var text = "message............ kdsafhjahd";
 	    var message = new Message(text, new Date().getTime());
 	    addMessage(user, message);
+	    addHtmlToUserArea(user.getUserHTML());
 	    
 	},2000);
 
 	function addHtmlToChatBox(html) {
 		$printer.append("<div class='message-html'>" + html + "<div>");
 		scrollBottom(); 
+	}
+
+	function addHtmlToUserArea(html) {
+		$userArea.append(html);
 	}
 
 	function addMessage(user, message) {
@@ -64,21 +77,30 @@ function Chat(user) {
 }
 
 
-
-function User(name, picture) {
-	this.name = name;
-	this.picture = picture;
+function User(user) {
+	this.name = user.name;
+	this.picture = user.picture;
+	this.status = user.status;
+	if (!user.status) {
+		this.status = "online";
+	}
 }
 
 User.prototype.getUserHTML = function() {
 	console.log("user html");
+
 	var html = '<div class="user-display-area"><div class="user-picture">'
-	+ '<img src="' + 
+	+ '<img class="' + getStatusClass(this.status) + '" src="' + 
 		this.picture +
 	'"></div><div class="username">' + this.name + '</div></div>';
 	console.log("html:" + html);
 	return html;
+	function getStatusClass(status) {
+		return status;
+	}
 }
+
+
 
 function Message(text, time) {
 	this.text = text;
@@ -98,6 +120,11 @@ Message.prototype.getMessageHTML = function() {
 }
 
 jQuery(document).ready(function() {
-	var user = new User("kamal Joshi", "http://gravatar.com/avatar/4cc30665303007ca1b503141d3f85858.jpg?d=monsterid");
-  	Chat(user);
+	var user = new User({name: "kamal Joshi", 
+		picture: "http://gravatar.com/avatar/4cc30665303007ca1b503141d3f85858.jpg?d=monsterid", 
+		status: "online"});
+  	Chat(user, function(msg) {
+  		console.log("New message: " + msg);
+  		//send to server
+  	});
 });
