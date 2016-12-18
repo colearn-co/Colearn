aeda08bedf702c15a8c5910442b25b1eac1279e8
spandey <sp.spaceforever@gmail.com>
2016-12-18 15:23:41 +0530

    added last seen, mobile swipe

--- app/assets/javascripts/util.js
+++ app/assets/javascripts/util.js
@@ -7,3 +7,19 @@ function addReferrerCookie() {
 function trackUser() {
 	addReferrerCookie();
 }
+
+
+
+(function(d){
+ var
+ ce=function(e,n){var a=document.createEvent("CustomEvent");a.initCustomEvent(n,true,true,e.target);e.target.dispatchEvent(a);a=null;return false},
+ nm=true,sp={x:0,y:0},ep={x:0,y:0},
+ touch={
+  touchstart:function(e){sp={x:e.touches[0].pageX,y:e.touches[0].pageY}},
+  touchmove:function(e){nm=false;ep={x:e.touches[0].pageX,y:e.touches[0].pageY}},
+  touchend:function(e){if(nm){ce(e,'fc')}else{var x=ep.x-sp.x,xr=Math.abs(x),y=ep.y-sp.y,yr=Math.abs(y);if(Math.max(xr,yr)>20){ce(e,(xr>yr?(x<0?'swl':'swr'):(y<0?'swu':'swd')))}};nm=true},
+  touchcancel:function(e){nm=false}
+ };
+ for(var a in touch){d.addEventListener(a,touch[a],false);}
+})(document);
+
