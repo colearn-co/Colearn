0435120fd22036fa42d997e010f82fae507bda28
spandey <sp.spaceforever@gmail.com>
2016-12-29 15:12:16 +0530

    added unsubscribed to chat followup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161225140020) do
+ActiveRecord::Schema.define(version: 20161229091229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -117,6 +117,12 @@ ActiveRecord::Schema.define(version: 20161225140020) do
   create_table "tags", force: :cascade do |t|
     t.string   "tag"
     t.text     "desc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "unsubscribes", force: :cascade do |t|
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
