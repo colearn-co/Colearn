8f5ad1f61230a9adb9d57113754a53a4ba90f5b3
kamal <kkjoshi.it@gmail.com>
2016-12-30 00:09:18 +0530

    schema.db changes

--- db/schema.rb
+++ db/schema.rb
@@ -110,17 +110,6 @@ ActiveRecord::Schema.define(version: 20161229091229) do
     t.integer "user_id"
   end
 
-  create_table "roles", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "roles_users", id: false, force: :cascade do |t|
-    t.integer "role_id"
-    t.integer "user_id"
-  end
-
   add_index "roles_users", ["role_id"], name: "index_roles_users_on_role_id", using: :btree
   add_index "roles_users", ["user_id"], name: "index_roles_users_on_user_id", using: :btree
 
