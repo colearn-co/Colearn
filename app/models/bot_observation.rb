cf3426c20bc5e81837588dc87e2920db83f8ab1c
kamal <kkjoshi.it@gmail.com>
2016-12-29 20:34:30 +0530

    chat messages improvements

--- app/models/bot_observation.rb
+++ app/models/bot_observation.rb
@@ -1,10 +1,14 @@
 class BotObservation < ActiveRecord::Observer
+	 include Rails.application.routes.url_helpers
 	observe :post, :suggestion, :invite
 
 	def after_create(record)
 		if record.class == Post
-			msg = "Congratulations your learning post is created. You can now start chatting! \r\n 
-				Share how you are going to start. This will help other members to sync up as soon as they join!"
+			msg = "Hi there #{record.user.name} :joy: . Your learning post (#{post_url(record)}) is created successfully  :book: 
+			I am default member of all chats :stuck_out_tongue_winking_eye: 
+			Go ahead and share how you are going to execute your leaning plans," + 
+			"so that other members gets synced with you as soon as they join.
+			Cheers. :thumbsup: :thumbsup: :thumbsup:"
 			record.create_user_chat(User.colearn_bot, {:message => msg})
 		elsif record.class == Suggestion
 			msg = "#{record.user.name} suggested: \r\n
@@ -16,9 +20,10 @@ class BotObservation < ActiveRecord::Observer
 	def after_update(record)
 		if record.class == Invite
 			if record.status_changed? && record.is_accepted?
-				msg = "Please join me to welcome #{record.user.name}!"
+				msg = "Welcome #{record.user.name} :sunglasses::sunglasses::sunglasses:"
 				record.post.create_user_chat(User.colearn_bot, {:message => msg})				
 			end			
 		end
 	end
 end
+
