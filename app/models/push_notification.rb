6e895b1373a6e31b0d8f150500a237a7633f4904
spandey <sp.spaceforever@gmail.com>
2017-01-26 16:20:06 +0530

    token updated on token change

--- app/models/push_notification.rb
+++ app/models/push_notification.rb
@@ -5,12 +5,34 @@ class PushNotification
         gcm = GCM.new(CONFIG["api_key"])
 		chat.chatable.members.each do |mem|
 			next if mem == chat.user || mem.is_online?(chat.chatable)
-			tokens = mem.device_tokens.map{|t| t.token}
+			devices = mem.device_tokens
+			tokens = devices.map{|t| t.token}
 			if !tokens.blank?
-				gcm.send(tokens, {:data => {:message => chat.notify_text, 
+				response = gcm.send(tokens, {:data => {:message => chat.notify_text, 
 					:redirect_url => Rails.application.routes.url_helpers.post_chats_path(chat.chatable, :auth_key => mem.user_auth_key, :uid => mem.id)}})
 				Rails.logger.info "gcm log: notification sent to #{mem.name}(#{mem.id}) with #{tokens.length} devices"
+				self.process_gcm_response(response, devices)
 			end
 		end
+	end
+
+	def self.process_gcm_response(response, devices)
+		begin
+            response = JSON.parse(response[:body])
+            puts "failed = ", response["failure"], "out of", devices.length
+            response["results"].each_with_index do |res, i|
+                if res["message_id"]
+                    if res["registration_id"]
+                        devices[i].token = res["registration_id"]
+                        devices[i].save!
+                    end
+                elsif res["error"] == "NotRegistered"
+                    devices[i].valid_token = false
+                    devices[i].save
+                end
+            end
+        rescue JSON::ParserError => e
+            ExceptionNotifier.notify_exception(e)
+        end
 	end
 end
