9eb2cd9061682cba26419200d446fd99075e9bcf
spandey <sp.spaceforever@gmail.com>
2017-01-29 12:42:58 +0530

    fixed select2 initialize

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,14 +1,14 @@
 class RegistrationsController < Devise::RegistrationsController
 
 
-	
+
   def update
   	super
     (params[:interests] || []).each do |interest|
     	begin
-    		current_user.interests.push(Interest.find_or_create_by(:title => interest))
+    		current_user.interests.push(Interest.find_or_create_by(:title => interest.capitalize))
     	rescue ActiveRecord::RecordNotUnique => e
-    		current_user.interests.push(Interest.find_or_create_by(:title => interest))
+    		current_user.interests.push(Interest.find_or_create_by(:title => interest.capitalize))
     	end
     end
   end
