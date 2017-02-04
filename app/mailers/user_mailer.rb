5661ec125c58a584fb972e0324ba61e763e7ff5e
kamal <kkjoshi.it@gmail.com>
2017-02-04 13:29:51 +0530

    leave learning email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,20 +5,20 @@ class UserMailer < ApplicationMailer
 		@post = post
 		@to = to
 		@from = from
-		mail(to: to.email, subject: "#{from.username} wants to join you")
+		mail(to: to.email, subject: "#{from.username} wants to join learning '#{post.title}'")
     end
 
     def join_confirmation_mail(to, post)      
 		@post = post
 		@to = to
-		mail(to: to.email, subject: "Colearn join confirmation")
+		mail(to: to.email, subject: 'Colearn join confirmation')
     end
 
     def join_rejection_mail(invite)      
 		@post = invite.post
 		@to = invite.user
 		@invite = invite
-		mail(to: @to.email, subject: "Colearn - Post join request rejected")
+		mail(to: @to.email, subject: 'Colearn - Post join request rejected')
     end
 
     def welcome_mail(to)
@@ -28,29 +28,35 @@ class UserMailer < ApplicationMailer
 
     def confirmation_mail(to)
         @user = to
-        mail(to: @user.email, subject: "Confirmation instructions")
+        mail(to: @user.email, subject: 'Confirmation instructions')
     end
 
     def post_chat_followup(user, post)
     	@user = user
     	@post = post
-    	mail(to: @user.email, subject: "[Colearn] New chat messages from learning post " + post.title)
+    	mail(to: @user.email, subject: '[Colearn] New chat messages from learning post ' + post.title)
     end
 
     def posts_summary(user, posts) 
     	@user = user
     	@posts = posts
-    	mail(to: @user.email, subject: "[Colearn] New learning requests")
+    	mail(to: @user.email, subject: '[Colearn] New learning requests')
     end
 
     def download_app(user)
         @user = user
-        mail(to: @user.email, subject: "[Colearn] Never miss an important message")
+        mail(to: @user.email, subject: '[Colearn] Never miss an important message')
     end
 
     def bug_update_mail(user)
         @user = user
-        mail(to: @user.email, subject: "[Colearn] We have discovered a bug in our chat on safari")
+        mail(to: @user.email, subject: '[Colearn] We have discovered a bug in our chat on safari')
+    end
+
+    def send_leave_post_mail_inactive_users(post, user)
+      @post = post
+      @user = user
+      mail(to: @user.email, subject: '[Colearn] We have noticed you are inactive on learning post ' + post.title)
     end
 
     def mail(*args)
